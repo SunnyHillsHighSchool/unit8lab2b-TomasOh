9919bf1d8ab5d2c59b5344fb767bfae536a8c136
Tomas Oh <>
2021-04-27 17:32:02 +0000

    Added Unit8Q2b

--- ProfNames.java
+++ ProfNames.java
@@ -1,9 +1,19 @@
 public class ProfNames
 {
+  //Method by Tomas Oh, Period 2
   public int countNames(String[][]nameArray, String lastName)
   {
-
-
-    return 0;
+    int count = 0;
+    for(int row = 0; row < nameArray.length; row++)
+    {
+      for(int col = 0; col < nameArray[0].length; col++)
+      {
+        if(nameArray[row][col].substring(0, lastName.length()).equals(lastName))
+        {
+         count++;
+        }
+      }
+    }
+   return count;
   }
 }
